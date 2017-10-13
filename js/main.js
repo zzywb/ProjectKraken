a043395454f04c912f5cde0430cfa30f784f8495
Kheyas <andreivettor@gmail.com>
2017-10-14 01:51:03 +0300

    Added FPS drawing + others
    * Added methods allowing the calculation and drawing of FPS
    * Added "use strict"
    * Added a check to see if the canvas is supported by the browser
    * Everything is now done in the game loop

--- js/main.js
+++ js/main.js
@@ -1,7 +1,91 @@
-(function(){
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    
-    ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 50, 50);
+'use strict';
+
+var delta;
+
+var fps = 0;
+var fpsUpdateInterval = 1000;
+var fpsTimer;
+var frames = 0;
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame || 
+    window.webkitRequestAnimationFrame   || 
+    window.mozRequestAnimationFrame      || 
+    window.oRequestAnimationFrame        || 
+    window.msRequestAnimationFrame       || 
+    function(callback, element){
+        window.setTimeout(function(){
+           
+            callback(+performance.now());
+
+        }, 1000 / 60);
+    };
 })();
+
+var lastRun;
+(function(window, document) {
+
+    var canvas   = document.getElementById('canvas'),
+        width        = canvas.width,
+        height       = canvas.height,
+        game_running = true,
+        show_fps     = true;
+
+    /**
+     * Determine if the canvas is supported, get the context and warn the user 
+     * otherwise.
+     */
+    if(canvas.getContext) {
+
+        var ctx = canvas.getContext('2d');
+
+    }
+    else {
+
+        // TODO: Design error message
+        alert('Your browser does not support the html5 canvas!');
+
+    }
+    
+    function showFPS() {
+
+        ctx.fillStyle = 'Black';
+        ctx.font = 'normal 16pt Arial';
+        ctx.fillText('FPS: ' + fps.toFixed(2), 10, 26);
+
+    }
+
+    function FPS(delta) {
+
+        fpsTimer += delta;
+        console.log(fpsTimer + " " + delta);
+        frames++;
+        if(fpsTimer > fpsUpdateInterval) {
+            fps = frames / fpsTimer;
+            fpsTimer -= fpsUpdateInterval;
+        }
+
+    }
+
+    (function gameLoop(){
+        
+        // Compute the time it took to render the last frame
+        delta = (performance.now() - lastRun);
+        
+        FPS(delta);
+
+        // Clear screen
+        ctx.clearRect(0, 0, width, height);
+
+        if(show_fps) showFPS();
+
+        if(game_running) requestAnimFrame(gameLoop);
+
+        ctx.fillStyle = 'green';
+        ctx.fillRect(10, 10, 50, 50);
+
+        lastRun = performance.now();
+
+    })();
+
+})(this, this.document);
