581f39f448508f102cc21644de070549843cebc7
Daniel Costrasel <gleesik.dark@gmail.com>
2017-10-14 21:59:06 +0300

    Fixed input handler
    Fixed Andrew bug

--- js/engine/input.js
+++ js/engine/input.js
@@ -6,19 +6,20 @@
 var currentKeysDown = [];
 
 document.addEventListener('keydown', function (e) {
-	currentKeysDown.push(e.key);
+	if(!currentKeysDown.includes(e.key))
+		currentKeysDown.push(e.key);
 }, true);
 
 document.addEventListener('keyup', function (e) {
-	if(currentKeysDown.includes(e.key)) {
-		currentKeysDown.splice(currentKeysDown.indexOf(e.key),1);
-	}
+	if(currentKeysDown.includes(e.key))
+		currentKeysDown.splice(currentKeysDown.indexOf(e.key), 1);
 }, true);
 
 var Input = new(function () {
 
 	this.getKeyDown = function (key) {
 
+		// console.log(currentKeysDown);
 		for (var i = 0; i < currentKeysDown.length; i++) {
 			if(keyMap[key].includes(currentKeysDown[i]))
 				return true;
