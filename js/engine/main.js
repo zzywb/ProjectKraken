497bf03a940a970350cf985a64175854be728a5d
Andrei Vettor <andreivettor@gmail.com>
2017-10-18 17:17:00 +0300

    Input Handler Refactoring
    * Input is now an object
    * The keyMap is back to work

--- js/engine/main.js
+++ js/engine/main.js
@@ -4,6 +4,14 @@
  * @since v1.0.0
 */
 
+var keyMap = {
+  fire1: [' ', 'Enter'],
+  up:    ['w', 'ArrowUp'],
+  down:  ['s', 'ArrowDown'],
+  left:  ['a', 'ArrowLeft'],
+  right: ['d', 'ArrowRight']
+};
 
 let fpsCounter = new FPSCounter(0, 0);
+let input = new Input(keyMap);
 let core = new Core(600, 400);
--- js/game/game.js
+++ js/game/game.js
@@ -2,10 +2,24 @@
 let rect = new Rectangle(30, 30, 50, 60, 'white', 5, 'blue');
 
 function update(delta) {
+
+  if(input.getKeyDown('up')) {
+    rect.y -= 5;
+  }
+  if(input.getKeyDown('down')) {
+    rect.y += 5;
+  }
+  if(input.getKeyDown('left')) {
+    rect.x -= 5;
+  }
+  if(input.getKeyDown('right')) {
+    rect.x += 5;
+  }
+  
 }
 
 function draw(context) {
-  // rect.draw(context);
+  rect.draw(context);
 }
 
 function loop(gameTime) {
